46987c42b455889264b33ccf6e8f042606675031
Borja Lafuente <borlafu@gmail.com>
2026-03-27 01:56:34 +0100

    Clean up: remove declaration files from src directory

--- src/commands.d.ts
+++
@@ -1,3 +0,0 @@
-import type { Message, Client } from 'whatsapp-web.js';
-export declare function handleCommand(message: Message, client: Client): Promise<void>;
-//# sourceMappingURL=commands.d.ts.map
